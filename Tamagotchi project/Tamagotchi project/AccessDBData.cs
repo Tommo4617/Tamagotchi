36cc5c39a84c98d527263b6aff275a7de137f7c5
Tommo4617 <AbbieT0406@gmail.com>
2025-07-08 16:54:31 +0100

    New user login stored successfully
    New data is added to the User table of the SQLite DB when a new user attempts to sign up. There is no validation checks on the data before inputted, but a successful connection to the DB is made which led to me being able to sign up and have my sign up details stored in the DB in the appropriate table (the User table)

---
+++ Tamagotchi project/Tamagotchi project/AccessDBData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Tamagotchi_project
+{
+    internal class AccessDBData
+    {
+        private string connectionString = "Data Source=TamagotchiDB.db;Version=3;";
+        public void AddUser(string name, string password)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                string InsertNewUser = "INSERT INTO User (Username, Password, Piggybank) VALUES (@name, @password, @piggybank)";
+                connection.Open();
+                Console.WriteLine("COnnection made!");
+                using (SQLiteCommand newUserCommand = new SQLiteCommand(InsertNewUser, connection)) {
+                    newUserCommand.Parameters.AddWithValue("@name", name);
+                    newUserCommand.Parameters.AddWithValue("@password", password);
+                    newUserCommand.Parameters.AddWithValue("@piggybank", 200);
+
+                    newUserCommand.ExecuteNonQuery();
+                }
+          
+                connection.Close();
+
+            }
+        }
+    }
+}
---
+++ Tamagotchi project/Tamagotchi project/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.IdentityModel.Tokens;
+using System.Data.SQLite;
+using Tamagotchi_project;
+
+//string connectionString = "Data Source=TamagotchiDB.db;Version=3;";
+//using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+//{
+//    connection.Open();
+//    Console.WriteLine("connection opened successfully.");
+//    CreateTable(connection);
+//    // Execute queries here
+
+//}
+
+Console.WriteLine("Welcome to Tamagotchi! \n1.Login \n2.Sign up \n3.Close");
+int selection = int.Parse(Console.ReadLine());
+switch (selection) {
+    case 1:
+        Console.WriteLine("Logging in...");
+        break;
+
+    case 2:
+        Console.WriteLine("Please enter a username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Please enter a password:");
+        string password = Console.ReadLine();
+        AccessDBData access = new AccessDBData();
+        access.AddUser(username, password);
+        break;
+
+    case 3:
+        Console.WriteLine("Closing the application...");
+        Environment.Exit(0);
+        break;
+}
+
+static void CreateTable(SQLiteConnection connection)
+{
+    string tableCmd = "CREATE TABLE IF NOT EXISTS User (UserID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Password TEXT, Piggybank INTEGER)";
+    using var cmd = new SQLiteCommand(tableCmd, connection);
+    cmd.ExecuteNonQuery();
+    Console.WriteLine("Table created (if not exists).");
+}
+
+static void InsertData(SQLiteConnection connection, string name)
+{
+    string insertCmd = "INSERT INTO User (Username), (Password), (Piggybank) VALUES (@name), (@Password), (@Piggybank)";
+    using var cmd = new SQLiteCommand(insertCmd, connection);
+    cmd.Parameters.AddWithValue("@name", name);
+    cmd.ExecuteNonQuery();
+    Console.WriteLine($"Inserted: {name}");
+}
+
+static void LoginSequence()
+{
+
+}
+
+static void SignUpSequence()
+{
+
+}
+
+
+    
+
+
